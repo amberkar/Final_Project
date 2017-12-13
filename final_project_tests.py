7a585d9050bbb730800773fe0c25435e27c6e64e
amberkar <amberkar@umich.edu>
2017-12-13 18:04:45 -0500

    added more tests

--- final_project_tests.py
+++ final_project_tests.py
@@ -10,7 +10,7 @@ class Test_events_caching(unittest.TestCase):
     self.assertTrue(read)
     self.assertIsInstance(read, str)
     cache_diction = json.loads(read)
-    self.assertIsInstance(cache_diction, dict)
+    self.assertIsInstance(cache_diction, dict, "testing that data is cached in events_cache_contents.json")
 
   def tearDown(self):
     self.cache_file.close()
@@ -24,7 +24,7 @@ class Test_formats_caching(unittest.TestCase):
     self.assertTrue(read)
     self.assertIsInstance(read, str)
     cache_diction = json.loads(read)
-    self.assertIsInstance(cache_diction, dict)
+    self.assertIsInstance(cache_diction, dict, "testing that data is cached in formats_cache_contents.json")
 
   def tearDown(self):
     self.cache_file.close()
@@ -38,10 +38,61 @@ class Test_categories_caching(unittest.TestCase):
     self.assertTrue(read)
     self.assertIsInstance(read, str)
     cache_diction = json.loads(read)
-    self.assertIsInstance(cache_diction, dict)
+    self.assertIsInstance(cache_diction, dict,"testing that data is cached in categories_cache_contents.json")
 
   def tearDown(self):
     self.cache_file.close()
+
+class Test_Token_Data(unittest.TestCase):
+  def setUp(self):
+    self.token_file = open("token.json", encoding='utf-8-sig')
+
+  def test_token_file(self):
+    read = self.token_file.read()
+    self.assertTrue(read)
+    self.assertIsInstance(read, str)
+    token_file = json.loads(read)
+    self.assertIsInstance(token_file, dict, "testing that data is cached in token.json")
+
+  def tearDown(self):
+    self.token_file.close()
+
+class Database_Test(unittest.TestCase):
+    def setUp(self):
+        try:
+            with open('events_cache_contents.json', 'r') as f:
+                EVENTS_CACHE_DICTION = json.loads(f.read())
+        except:
+            EVENTS_CACHE_DICTION = {}
+
+        try:
+            with open('formats_cache_contents.json', 'r') as f:
+                FORMATS_CACHE_DICTION = json.loads(f.read())
+        except:
+            FORMATS_CACHE_DICTION = {}
+        try:
+            with open('categories_cache_contents.json', 'r') as f:
+                CATEGORIES_CACHE_DICTION = json.loads(f.read())
+        except:
+            CATEGORIES_CACHE_DICTION = {}
+        try:
+            with open('tests_token.json', 'r') as f:
+                token_dict = json.loads(f.read())
+        except:
+            token_dict = {}
+
+    def test_formats_table(self):
+        self.data = formats_data['formats']
+        self.assertTrue(isinstance(self.data, list))
+
+    def test_categories_table(self):
+        self.data2 = categories_data['categories']
+        self.assertTrue(isinstance(self.data2, list))
+
+    def test_events_table(self):
+        self.data3 = events_data['events']
+        self.assertTrue(isinstance(self.data3, list))
+
 
 
 if __name__ == "__main__":
